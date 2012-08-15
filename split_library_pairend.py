c2c7525cd94193e8846c9b99ad4da0d4cdebe0d9
yu68 <yu68@sysbio.igb.uiuc.edu>
2012-08-15 15:40:01 -0500

    update for pairend data

--- split_library_pairend.py
+++ split_library_pairend.py
@@ -3,12 +3,13 @@ from Bio import SeqIO
 
 def ParseArg():
     '''Parse the argument'''
-    p=argparse.ArgumentParser( description = 'Example: %(prog)s -q example.fastq -b barcode.txt', epilog = 'Library dependency: Bio')
+    p=argparse.ArgumentParser( description = 'Example: %(prog)s -q example.F1.fastq example.R1.fastq -b barcode.txt', epilog = 'Library dependency: Bio')
     group=p.add_mutually_exclusive_group()
     group.add_argument("-f","--fasta",action='store_true',help='add this option for fasta input file')
     group.add_argument("-q","--fastq",action='store_true',help='add this option for fastq input file')
     p.add_argument('-v','--version',action='version',version='%(prog)s 0.1')
-    p.add_argument('input',type=str,help='input fastq/fasta file')
+    p.add_argument('input1',type=str,help='input fastq/fasta file 1 for pairend data (contain barcodes)')
+    p.add_argument('input2',type=str,help='input fastq/fasta file 2 for pairend data')
     p.add_argument('-b','--barcode',dest='barcode',type=str,help='barcode file')
     p.add_argument('-t','--trim',action='store_true',help='trim sequence before and within barcode')
     p.add_argument('-m','--max_score',dest='max_score',type=int,default=2, help="max(mismatch+indel) allowed for barcode match, otherwise move reads into 'unassigned' file") 
@@ -61,24 +62,31 @@ print (fuzzy_substring("ACTAAC", "ACTAACTAGCCATGCAATGGCTAG"))
 
 def Main():
     args=ParseArg()
+
     if args.fastq:
        type="fastq"
     elif args.fasta:
        type="fasta"
-    seq={}
+
+    seq1={}
+    seq2={}
     #----------- read barcode ----------
     barcodes=[]
     for i in open(args.barcode,'r'):
         i=i.strip()
         barcodes.append(i)
         barcode_len=len(i)
-        seq[i]=[]
-    seq['unassign']=[]
+        seq1[i]=[]
+        seq2[i]=[]
+    seq1['unassign']=[]
+    seq2['unassign']=[]
     #-----------------------------------
+
+    records2=SeqIO.parse(open(args.input2,"rU"),type)
     
     print "start to assign sequence to different barcodes..."    
     print "----------"
-    for record in SeqIO.parse(args.input,type):
+    for record in SeqIO.parse(args.input1,type):
         miScore=barcode_len
         for i in barcodes:
             score,j=fuzzy_substring(i,record.seq)
@@ -86,18 +94,23 @@ def Main():
                 barcode=i
                 end=j
                 miScore=score
+        record2=records2.next()
         if miScore>args.max_score:
-            seq['unassign'].append(record)
+            seq1['unassign'].append(record)
+            seq2['unassign'].append(record2)
         else:
           if args.trim:
-              seq[barcode].append(record[end:])
+              seq1[barcode].append(record[end:])
           else:
-              seq[barcode].append(record)
+              seq1[barcode].append(record)
+          seq2[barcode].append(record2)
     barcodes.append('unassign')
     for barcode in barcodes:
-        print "#Writing "+type+" file for barcode: "+barcode+". # of seq: %i ."%len(seq[barcode])
-        output=open(barcode+args.input,'w')
-        SeqIO.write(seq[barcode],output,type)
+        print "#Writing "+type+" files for barcode: "+barcode+". # of seq: %i ."%len(seq1[barcode])
+        output1=open(barcode+args.input1,'w')
+        output2=open(barcode+args.input2,'w')
+        SeqIO.write(seq1[barcode],output1,type)
+        SeqIO.write(seq2[barcode],output2,type)
 
         
 if __name__=="__main__":
